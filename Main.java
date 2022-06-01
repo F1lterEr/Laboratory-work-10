ef73ee79234bde1875e5397003e9b9d20fce3a70
F1lterEr <98636299+F1lterEr@users.noreply.github.com>
2022-06-01 12:41:33 +0300

    Second

--- Main.java
+++ Main.java
@@ -7,39 +7,12 @@ public class Main {
         System.out.println("Введите строку: ");
         String str = scanner.nextLine();
         String str2 = str.replace(" ", "");
-        String str3 = str2.toLowerCase();
-        System.out.println("Ваша строка: " + str);
-        int length = str3.length();
-        char[] c = str3.toCharArray();
-        int result = 0;
-        int k = length - 1;
-        if(length % 2 != 0){
-            int a = (length / 2) + 1;
-            for(int i = 0; i < a; i++, k--){
-                if(c[i] == c[k]){
-                    result++;
-                }
-            }
-            if(result == (length / 2) + 1){
-                System.out.println("Строка палиндром.");
-            }
-            else{
-                System.out.println("Строка не палиндром.");
-            }
+        StringBuilder sb = new StringBuilder(str2);
+        if (str2.equals(sb.reverse().toString())){
+            System.out.println("Строка палиндром");
         }
-        if(length % 2 == 0){
-            int a = length / 2;
-            for(int i = 0; k >= a; i++, k--){
-                if(c[i] == c[k]){
-                    result++;
-                }
-            }
-            if(result == length / 2){
-                System.out.println("Строка палиндром.");
-            }
-            else{
-                System.out.println("Строка не палиндром.");
-            }
+        else {
+            System.out.println("Строка не палиндром");
+        }
         }
     }
-}
